bb16070abf51dc74e40a47d08cf33e09c6cbfacd
Mostapha boutrhaline <120510393+mostaphabr@users.noreply.github.com>
2024-11-02 13:49:17 +0100

    falling animation

--- files/script.js
+++ files/script.js
@@ -142,3 +142,27 @@ document.querySelectorAll(".myLink").forEach(element => {
 
 
 // Prevent triggering the modal
+
+
+
+function stars() {
+    let e = document.createElement("div");
+    let size = Math.random() * 12;
+    let duration = Math.random() * 3;
+  
+    e.setAttribute("class", "star");
+    document.body.appendChild(e);
+  
+    e.style.left = Math.random() * +(innerWidth-100) + "px";
+    e.style.fontSize = 12 + size + "px";
+    e.style.animationDuration = 4 + duration + "s";
+  
+    setTimeout(function () {
+      document.body.removeChild(e);
+    }, 5000);
+  }
+  
+  setInterval(function () {
+    stars();
+  }, 130);
+  
